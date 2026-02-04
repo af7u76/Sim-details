91e902d3123d0f61c47411603c901feff43fff41
af7u76 <af7u76@gmail.com>
2026-02-05 02:36:21 +0500

    Proxy.php

--- Proxy.php
+++ Proxy.php
@@ -1,1 +1,20 @@
+<?php
 
+if(!isset($_GET['search'])){
+    exit("No Input");
+}
+
+$search = $_GET['search'];
+
+$url = "https://paksimdetails.xyz/K/Zawar.php?search=".$search."&type=mobile";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
+?>
